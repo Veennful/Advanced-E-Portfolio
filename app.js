024c7f8bde7c7ecd139063cb0ef8049c986164db
Valance Niambi Taylor "Vee <136548569+Veennful@users.noreply.github.com>
2024-06-12 00:40:51 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -22,9 +22,16 @@ function contact(event) {
             loading.classList.remove("modal__overlay--visible");
             alert(
                 "The email service is temporarily unavailable. Please email me directly at valancentaylor@gmail.com"
-            )
+            );
         })
-    setTimeout(() => {
-        console.log('it worked!')
-    }, 1000);
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
