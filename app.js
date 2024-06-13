9e68c1a815923fe686dba5c7eccdf31699110705
Valance Niambi Taylor "Vee <136548569+Veennful@users.noreply.github.com>
2024-06-12 23:49:05 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-// template_e8apbod
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
-// v0--psBoSqo8SYu0F
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 === 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
 
-// service_etw17uo
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
     event.preventDefault()
@@ -26,7 +46,7 @@ function contact(event) {
         })
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
